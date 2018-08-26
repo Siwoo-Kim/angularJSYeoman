1e07ddf930fe673cba3ef5d41243f98e3c9ede87
Siwoo Kim <skim327@myseneca.ca>
2018-08-26 22:01:10 +0900

    first commit, Yeoman basic structure

---
+++ src/main/java/com/siwoo/angularlabtemp/AngularlabtempApplication.java
@@ -0,0 +1,12 @@
+package com.siwoo.angularlabtemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularlabtempApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngularlabtempApplication.class, args);
+    }
+}
---
+++ src/test/java/com/siwoo/angularlabtemp/AngularlabtempApplicationTests.java
@@ -0,0 +1,16 @@
+package com.siwoo.angularlabtemp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AngularlabtempApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
